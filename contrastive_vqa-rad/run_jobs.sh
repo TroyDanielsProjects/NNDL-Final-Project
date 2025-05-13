32cad5d63269e625a91399f9527f32ba2e8e8762
Christopher Demirjian <cjd2186@columbia.edu>
2025-05-12 23:55:34 -0400

    Working Test Sandwich

---
+++ contrastive_vqa-rad/run_jobs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes False
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
+python run_experiments.py --noise none --dupes True
