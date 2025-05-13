8448eecb31f93f1f092994d9d31e3b6733a401e8
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-05-13 01:11:08 -0400

    update

--- contrastive_vqa-rad/run_experiments.py
+++ contrastive_vqa-rad/run_experiments.py
@@ -9,7 +9,7 @@ NOISE_MODES = ["none", "text", "image", "both"]
 
 
 
-def main(dupes, epochs, batch_size):
+def main(dupes, epochs, batch_size, repeats):
     # set up logging
     log_filename = f"logs/{datetime.now()}.log"
     os.makedirs("logs", exist_ok=True)
@@ -31,29 +31,44 @@ def main(dupes, epochs, batch_size):
     device = 'cuda' if torch.cuda.is_available() else "cpu"
     logger.info(f"Using {device} device")
     logger.info(dupes)
-    for mode in NOISE_MODES:
-        logger.info(f"\n\n=== Training with noise_mode = '{mode}' ===")
-        # 1) Prepare data loaders for this regime
-        creator = Data_Creater(noise_mode=mode, batch_size=batch_size)
-        #specify dupes
-        train_dl, val_dl, test_dl = creator.create_datasets(dupes)
-        
-        #debug: test datasets for noise
-        #debug: sample=next(iter(train_dl))
-        #debug:print(sample)
+    total_accuracy_difference_none = 0
+    total_accuracy_difference_text = 0
+    total_accuracy_difference_image = 0
+    total_accuracy_difference_both = 0
+    for i in repeats:
+        logger.info(f"Starting iteration {i+1}")
+        for mode in NOISE_MODES:
+            logger.info(f"\n\n=== Training with noise_mode = '{mode}' ===")
+            # 1) Prepare data loaders for this regime
+            creator = Data_Creater(noise_mode=mode, batch_size=batch_size)
+            #specify dupes
+            train_dl, val_dl, test_dl = creator.create_datasets(dupes)
+            
+            #debug: test datasets for noise
+            #debug: sample=next(iter(train_dl))
+            #debug:print(sample)
 
-        # 2) Build model & trainer
-        model   = ConstrastiveModel().to(device)
-        trainer = Trainer(model, train_dl, val_dl, device)
+            # 2) Build model & trainer
+            model   = ConstrastiveModel().to(device)
+            trainer = Trainer(model, train_dl, val_dl, device)
 
-        # 3) Train & save
-        trainer.train(epochs=epochs)
-        if dupes:
-            model_name=f"models/clip_{mode}_Dupes.pth"
-        else:
-            model_name=f"models/clip_{mode}_NoDupes.pth"
-        trainer.test()
-        trainer.save_model(path=model_name)
+            # 3) Train & save
+            start_accuracy = trainer.test()
+            trainer.train(epochs=epochs)
+            end_accuracy = trainer.test()
+            diff = end_accuracy - start_accuracy
+            if mode == 'none':
+                total_accuracy_difference_none+=diff
+                logger.info(f"For {mode} - difference in train-original:{diff}, total={total_accuracy_difference_none}")
+            elif mode == 'text':
+                total_accuracy_difference_text+=diff
+                logger.info(f"For {mode} - difference in train-original:{diff}, total={total_accuracy_difference_text}")
+            elif mode == 'image':
+                total_accuracy_difference_image+=diff
+                logger.info(f"For {mode} - difference in train-original:{diff}, total={total_accuracy_difference_image}")
+            else:
+                total_accuracy_difference_both+=diff
+                logger.info(f"For {mode} - difference in train-original:{diff}, total={total_accuracy_difference_both}")
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -67,15 +82,20 @@ def build_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--epochs", type=int,
         help=f"How many epochs do you want to run per experiment",
-        default=15)
+        default=20)
     
     parser.add_argument(
         "--batch_size", type=int,
         help=f"Set the batch size for each experiment",
         default=64)
     
+    parser.add_argument(
+        "--repeats", type=int,
+        help=f"Set the batch size for each experiment",
+        default=10)
+    
     return parser
 
 if __name__ == "__main__":
     args= build_parser().parse_args()
-    main(eval(args.dupes), args.epochs, args.batch_size)
+    main(eval(args.dupes), args.epochs, args.batch_size, args.repeats)
